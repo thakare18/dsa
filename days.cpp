4bccdbd3e157c785a0d960a79950965236ad65db
Prathamesh Vinayak Thakare <prathamthackeray18@gmail.com>
2025-12-22 23:31:32 +0530

    dsa

---
+++ days.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //nested if-else statement
+int index = 7;
+
+if(index==1){
+ cout<<"monday"<<endl;
+}
+else if (index==2){
+ cout<<"tuesday"<<endl;
+}
+else if (index==3){
+ cout<<"wednesday"<<endl;
+}
+else if (index==4){
+ cout<<"thursday"<<endl;
+}
+else if (index==5){
+ cout<<"friday"<<endl;
+}
+else if (index==6){
+ cout<<"saturday"<<endl;
+}
+else if(index==7){
+ cout<<"sunday"<<endl;
+}
+
+ return 0;
+}
---
+++ ternary.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+//     int age=10;
+//    int value = (age>18) ? 100 : 200;
+//     cout << "Value: " << value << endl;
+
+int index = 78;
+switch (index){
+    case 1: cout<<"monday"<<endl;
+    break;
+    case 2: cout<<"tuesday"<<endl;
+    break;
+    case 3: cout<<"wedenday"<<endl;
+    break;
+    case 4: cout<<"thursday"<<endl;
+    break;
+    case 5: cout<<"friday"<<endl;
+    break;
+    case 6: cout<<"saturday"<<endl;
+    break;
+    case 7: cout<<"sunday"<<endl;
+    break;
+    default: cout<<"babbarday"<<endl;
+    return 0;
+}
+}
+
+// This program demonstrates the use of the ternary operator in C++. It checks if the variable 'age' is greater than 18.
+//
