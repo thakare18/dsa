58258bae02ea83dbe334417c019b440d224a597e
Prathamesh Vinayak Thakare <prathamthackeray18@gmail.com>
2026-01-13 18:11:35 +0530

    Add square.cpp to print a pattern of stars

---
+++ square.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n=3 , m = 5;
+     for(int row =1; row <= n ; row++){
+// for each row we need to print m stars
+        
+        for(int col=1; col<=5; col++){
+            cout<< "* ";
+        } 
+        cout << endl;
+     } 
+    
+}
