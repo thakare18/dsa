132e8737a3988d0217fdc38f05e31a99787b980e
Prathamesh Thakare <prathamthackeray18@gmail.com>
2026-04-04 00:49:11 +0530

    comments

--- 867. Transpose Matrix.cpp
+++ 867. Transpose Matrix.cpp
@@ -18,3 +18,47 @@ public:
         
     }
 };
+
+
+
+
+// Transpose of a Matrix
+// Row becomes column and column becomes row
+
+#include <vector>
+using namespace std;
+
+// class Solution {
+// public:
+//     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+
+//         // number of rows in original matrix
+//         int rowsInOriginalArray = matrix.size();
+
+//         // number of columns in original matrix
+//         int colsInOriginalArray = matrix[0].size();
+
+//         // transpose:
+//         // rows -> columns
+//         // columns -> rows
+//         int rowsOfTransposeMatrix = colsInOriginalArray;
+//         int colsOfTransposeMatrix = rowsInOriginalArray;
+
+//         // create transpose matrix with swapped dimensions
+//         vector<vector<int>> transpose(
+//             rowsOfTransposeMatrix,
+//             vector<int>(colsOfTransposeMatrix, 0)
+//         );
+
+//         // logic: i,j -> j,i
+//         for(int i = 0; i < rowsOfTransposeMatrix; i++) {
+//             for(int j = 0; j < colsOfTransposeMatrix; j++) {
+
+//                 // swap row and column index
+//                 transpose[i][j] = matrix[j][i];
+//             }
+//         }
+
+//         return transpose;
+//     }
+// };
